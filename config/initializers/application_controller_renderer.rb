b4538a279fca1ee763f225eeff4d8c63f176ec01
Yuki-Ichigo <bzpl1215@gmail.com>
2020-06-30 00:00:13 +0900

    課題3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2bc1d1a89e94f2dca7b2f058c7e14aa0eb73a2c6923aa41380a8310c18f0bd0deb3fe80e3124994e74dee58e16ea1758c46cc9c6e3f3fb2ad94a986f741e19c3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :books, only: [:show, :index, :edit, :update, :create, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
